0d9cd197787dc812665e40f5e9f71088b26e49e0
Vlad Stahnyuk <stahnukvlad21@gmail.com>
2026-01-20 11:17:17 +0200

    chore: fix case-duplicate app dirs and track migrations

--- Project/settings.py
+++ Project/settings.py
@@ -123,9 +123,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
     ],
-    'DEFAULT_SCHEMA_CLASS': [
-        'drf_spectacular.openapi.AutoSchema',
-    ],
+    'DEFAULT_SCHEMA_CLASS':
+        'drf_spectacular.openapi.AutoSchema'
+    ,
     'DEFAULT_PERMISSION_CLASSES': [
           'rest_framework.permissions.IsAuthenticated',
     ],
