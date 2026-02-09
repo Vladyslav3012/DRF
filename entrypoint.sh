d17048586c53a7b8e93cd8186007c5b81776d065
Vlad Stahnyuk <stahnukvlad21@gmail.com>
2026-02-09 16:24:57 +0200

    try fix celery heartbeat

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-celery -A Project worker --loglevel=info --concurrency=1 &
+celery -A Project worker --loglevel=info --pool=solo &
 
 uvicorn Project.asgi:application --host 0.0.0.0 --port 8000
