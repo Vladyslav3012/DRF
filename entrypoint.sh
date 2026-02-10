98f0df010f9ceb84b0dfd6e839ba3862d12dbf89
Vlad Stahnyuk <stahnukvlad21@gmail.com>
2026-02-10 15:10:17 +0200

    try fix celery

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-celery -A Project worker --loglevel=info --pool=solo &
+celery -A Project worker --loglevel=info --pool=solo --concurrency=1 &
 
 uvicorn Project.asgi:application --host 0.0.0.0 --port 8000
