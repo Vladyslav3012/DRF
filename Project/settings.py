80fb0f73bf6a58ded87f3f1078e0f9156548ded6
Vlad Stahnyuk <stahnukvlad21@gmail.com>
2026-02-10 11:37:43 +0200

    fix last commit

--- Project/settings.py
+++ Project/settings.py
@@ -5,11 +5,12 @@ from dotenv import load_dotenv
 import dj_database_url
 import socket
 
+old_getaddrinfo = socket.getaddrinfo
 
-def getaddrinfo_ipv4(host, port, family=0, type=0, proto=0, flags=0):
-    return socket.getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
+def new_getaddrinfo(host, port, family=0, type=0, proto=0, flags=0):
+    return old_getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
 
-socket.getaddrinfo = getaddrinfo_ipv4
+socket.getaddrinfo = new_getaddrinfo
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
